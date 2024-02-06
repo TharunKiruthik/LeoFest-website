4f06d748c07539631b42af3cdc78887c50719cb3
TharunKiruthik <120109765+TharunKiruthik@users.noreply.github.com>
2024-02-07 00:08:30 +0530

    links added

--- script.js
+++ script.js
@@ -28,4 +28,29 @@ document.addEventListener('DOMContentLoaded', function () {
       },
     });
   });
+ // Select HTML elements by their IDs and assign them to variables
+ const main = () => {
+  const second = 1000
+  const minute = second * 60
+  const hour = minute * 60
+  const day = hour * 24
+      
+  //INSERT EVENT DATE AND TIME HERE IN THIS FORMAT: 'March 20, 2024, 00:00:00'
+  const EVENTDATE = new Date('March 20, 2024, 00:00:00')
   
+  const countDown = new Date(EVENTDATE).getTime()
+  const x = setInterval(() => {
+      
+      const now = new Date().getTime()
+      const distance = countDown - now
+      
+      document.getElementById("days").innerText = Math.floor(distance / day)
+      document.getElementById("hours").innerText = Math.floor((distance % day) / (hour))
+      document.getElementById("minutes").innerText = Math.floor((distance % hour) / (minute))
+      document.getElementById("seconds").innerText = Math.floor((distance % minute) / second)
+      
+      //delay in milliseconds
+  }, 0)
+}
+
+main();
